e60435151e1b5960761325c20936d6a4e1a34520
Pappu Sharma <ro987456sharma@gmail.com>
2025-09-17 18:23:42 +0530

    PS - BitCare Folder Structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello Bitcare
+      </h1>
+      <div>
+      <p className='text-gray-700'>Welcome to Bitcare EMR System Folder Structure</p>
+      <p className='font-bold text-2xl'>This is a simple Vite + React + TypeScript application.</p>
+      </div>
+    </>
+  )
+}
+
+export default App

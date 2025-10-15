697e4808abfabeaac221092321ff1f7c0190b94e
Pappu Sharma <ro987456sharma@gmail.com>
2025-10-15 15:36:33 +0530

    PS - Login path

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,16 @@
 import {  Navigate } from "react-router-dom";
-import Login from "../features/Login/login";
+import Login from "../features/Login/component/login";
 import Dashboard from "../features/component/Dashboard";
 import Registration from "../pages/RegistrationPage";
 import MainLayout from "../layout/main/MainLayout";
 import BlankLayout from "../layout/main/BlankLayout";
 import Users from "../features/component/Users";
-import Profile from "../pages/Clinic/ProfilePage";
+// import Profile from "../pages/Clinic/ProfilePage";
 import DoctorProfile from "../features/component/DocProfile";
 import DocDashboard from "../features/component/DocDashboard";
 import ResetPasswordPage from "../components/common/ResetPasswordPage";
+import Profile from "../features/clinic/components/Profile";
+import ManageAvailabilityPage from "../features/clinic/components/ManageAvalibility";
 
 
 const Router = [
@@ -31,6 +33,7 @@ const Router = [
       { path: '/clinic-settings', element: <Profile /> },
       { path: '/profile', element: <DoctorProfile /> },
       { path: '/docdashboard', element: <DocDashboard /> },
+      {path:'/clinic-manage', element:<ManageAvailabilityPage/>}
 
     ]
   }

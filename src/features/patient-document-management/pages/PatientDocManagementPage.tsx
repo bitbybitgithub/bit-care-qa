79f6473c106ab526f592508cadecbdfdb5d27408
Adil <adilkoduru012@gmail.com>
2025-12-10 11:13:02 +0530

    Ak(minorissue)

--- src/features/patient-document-management/pages/PatientDocManagementPage.tsx
+++ src/features/patient-document-management/pages/PatientDocManagementPage.tsx
@@ -5,7 +5,6 @@ import UploadDocument from "../pages/UploadDocument";
 import ExistingDocumentList from "../pages/ExistingDocumentList";
 
 export default function PatientDocManagementPage() {
-  const;
   return (
     <div className="flex flex-col justify-between gap-4">
       <div className="w-full h-32 bg-[var(--color-white)] shadow-md rounded-md p-4">

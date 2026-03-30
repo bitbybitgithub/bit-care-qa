bfe2e65db225d4d7e81e914c319835c36eada106
Pappu Sharma <ro987456sharma@gmail.com>
2026-03-30 15:31:34 +0530

    PS - bugs fixed

--- src/services/EmrApi.ts
+++ src/services/EmrApi.ts
@@ -4,7 +4,7 @@ import type { RefreshToken } from "../types/types";
 import { getSocket } from "../context/socket";
 
 export const BASE_URL = "http://qacliniccareapi.bitbybitsolutions.co.in/api";
-//export const BASE_URL = "http://qacliniccareapi.bitbybitsolutions.co.in/api";
+//export const BASE_URL = "https://cliniccareapi.bitbybitsolutions.co.in/api";
 // export const BASE_URL = "http://localhost:8989/api";
 
 // -------------------- //

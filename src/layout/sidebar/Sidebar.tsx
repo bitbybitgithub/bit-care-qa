178e2efeedce871092830b226d64d700fbba3c4a
Ganesh907 <ganishinde907@gmail.com>
2026-04-09 12:15:25 +0530

    GS (Map Entity Logo)

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { Drawer, IconButton, useMediaQuery } from "@mui/material";
 import { Menu as MenuIcon } from "@mui/icons-material";
 import Logo1 from "../../assets/BitCareLogo.png";
-import Logo2 from "../../assets/BitCareLogo2.png";
 import { getSessionItem } from "../../context/sessions/userSession";
 import type { Role, SidebarProps } from "../../types/common/sidebarTypes";
 import type { EntityType } from "../../context/constant/enum";
 import { SIDEBAR_MENUS } from "../../context/constant/sidebarMenus";
+
 const Sidebar: React.FC<SidebarProps> = ({
   isCollapsed,
   setIsCollapsed,
@@ -19,12 +19,19 @@ const Sidebar: React.FC<SidebarProps> = ({
 
   const [role, setRole] = React.useState<Role | null>(null);
   const [entityType, setEntityType] = React.useState<EntityType | null>(null);
+  const [entitylogo, setEntitylogo] = useState(Logo1);
 
   React.useEffect(() => {
     setRole(getSessionItem("user", "role"));
     setEntityType(getSessionItem("user", "entity_type"));
-  }, []);
+    const logo = getSessionItem("user", "entity_logo");
 
+    if (logo) {
+      setEntitylogo("data:image/png;base64," + logo);
+    } else {
+      setEntitylogo(Logo1);
+    }
+  }, []);
   const menuItems =
     role && entityType
       ? SIDEBAR_MENUS[entityType as EntityType]?.[role] ?? []
@@ -41,20 +48,14 @@ const Sidebar: React.FC<SidebarProps> = ({
       transition-all duration-300
       ${isCollapsed ? "w-20" : "w-56"}`}
     >
-      <div className="p-2 flex justify-center">
-        {!isCollapsed ? (
-          <img
-            src={Logo1}
-            alt="Logo"
-            className="w-[80%] h-[70px] object-contain bg-white shadow-2xl rounded"
-          />
-        ) : (
-          <img
-            src={Logo2}
-            alt="Logo"
-            className="w-10 h-10 bg-white rounded-full p-1"
-          />
-        )}
+      <div className="p-2 bg-[var(--color-primary-dark)] flex justify-center items-center h-16 border-b-2 border-b-[var(--color-primary-dark)]">
+        <img
+          src={entitylogo}
+          alt="Logo"
+          className={`object-contain bg-white w-[90%] p-1 shadow-[var(--shadow-md)] rounded transition-all duration-300
+      ${isCollapsed ? "h-14" : "h-14"}
+    `}
+        />
       </div>
 
       <nav className="flex-1 overflow-y-auto">
@@ -71,10 +72,9 @@ const Sidebar: React.FC<SidebarProps> = ({
                   className={`flex items-center gap-3 px-3 py-2 rounded-md
                     text-sm font-medium transition
                     ${isCollapsed ? "justify-center" : ""}
-                    ${
-                      active
-                        ? "bg-white text-[var(--color-primary)]"
-                        : "text-white hover:bg-white hover:text-[var(--color-primary)]"
+                    ${active
+                      ? "bg-white text-[var(--color-primary)]"
+                      : "text-white hover:bg-white hover:text-[var(--color-primary)]"
                     }
                   `}
                 >

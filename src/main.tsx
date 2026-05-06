27cedc055296a7460c3e5bc02a2b53ee0a1c9099
mangalsinghrawat <harishrawat457@gmail.com>
2026-05-06 11:35:58 +0530

    MR - Clinic Profile Setting Page Modified

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,14 @@ import "./theme/token.css";
 import "./index.css";
 import { LoaderProvider } from "./context/LoaderContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
     <Provider store={store}>
       <BrowserRouter>
         <ThemeProviderWrapper>
@@ -25,5 +28,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </ThemeProviderWrapper>
       </BrowserRouter>
     </Provider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
